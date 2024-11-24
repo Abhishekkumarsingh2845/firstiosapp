a677c4b8b3a5adf0cf8d17cf3a6c73a78fda64fd
Abhishekkumarsingh2845 <singhrnq231217@gmail.com>
2024-11-24 14:18:34 +0530

    added literal

--- firstiosapp/main.swift
+++ firstiosapp/main.swift
@@ -5,7 +5,7 @@ print(aa)
 
 
 let bb="aaaa"
-
+let abb = 55, b = 1, c = 15
 print(bb)
 
 // let is constant it cannot be changed
@@ -32,3 +32,21 @@ let a = "counting letter in the "
 print(a.count)
 
 
+//hasPrefix ans hasSuffix
+let qq = "he is good"
+print(qq.hasPrefix("he"))
+print(qq.hasSuffix("he"))
+
+
+
+//Type Annotation-->Type annotation means mentioning the type of constant or variable when declared
+var stringName : String
+var numberTemp : Int
+
+//literal
+
+
+let q=32 //interger literal
+let qq=32.1 //floating literal
+let qqq="fff" //string literal
+let qqqq=true //boolean literal
